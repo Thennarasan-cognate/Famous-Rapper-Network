1be795e0fdd61214decdedaddfdf02585b061fd7
Thennarasan-cognate <85215492+Thennarasan-cognate@users.noreply.github.com>
2021-08-30 18:50:42 +0530

    Initial change

---
+++ Member-Login.php
@@ -0,0 +1,191 @@
+<?php session_start(); ?>
+<?php include "db.php"; ?>
+
+<?php
+    
+    if(isset($_REQUEST['submit'])){
+         
+//        $email    = $_REQUEST['email'];
+        $username    = $_REQUEST['username'];
+        $password = $_REQUEST['password'];
+                        
+        $password = mysqli_real_escape_string($connection,$_POST['password']);
+        $password = md5($password);  
+    
+        $query = "SELECT * FROM register WHERE username = '{$username}' ";
+        $select_register_query = mysqli_query($connection, $query);
+        
+        if(!$select_register_query){
+            
+            die("Query Failed" . mysqli_error($connection));
+            
+        }
+          
+          while($row = mysqli_fetch_array($select_register_query)){
+              
+               $db_id = $row['id'];
+               $db_username = $row['username'];
+               $db_email = $row['email'];
+               $db_password = $row['password'];
+               $db_firstname = $row['firstname'];
+               $db_lastname = $row['lastname'];
+               $db_image = $row['image'];
+              
+          }
+        
+        
+//        if($email === $db_email){
+        if($username === $db_username){
+        if($password === $db_password){
+     
+            
+             $_SESSION['email'] = $db_email;
+             $_SESSION['username'] = $db_username;
+             $_SESSION['firstname'] = $db_firstname;
+             $_SESSION['lastname'] = $db_lastname;
+             $_SESSION['image'] = $db_image;
+
+// header("Location:profile.php");
+            echo "login Successful";
+           
+        }else{
+            
+            $message_password = "Incorrect password";
+        }
+         
+        }else{
+            $message_username = "Username is incorrect";   
+              
+        }
+        
+        }
+?>  
+
+
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="INTUITIVE">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Member Login</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Member-Login.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.23.2, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "Site2",
+		"logo": "images/default-logo.png"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Member Login">
+    <meta property="og:description" content="">
+    <meta property="og:type" content="website">
+  </head>
+  <body class="u-body"><header class="u-clearfix u-header u-header" id="sec-3ee6"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="https://nicepage.com" class="u-image u-logo u-image-1">
+          <img src="images/default-logo.png" class="u-logo-image u-logo-image-1">
+        </a>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</symbol>
+</defs></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Home.html" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="About.html" style="padding: 10px 20px;">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Contact.html" style="padding: 10px 20px;">Contact</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Member-Login.php" style="padding: 10px 20px;">Member Login</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Home.html" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="About.html" style="padding: 10px 20px;">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Contact.html" style="padding: 10px 20px;">Contact</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Member-Login.html" style="padding: 10px 20px;">Member Login</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header>
+    <section class="u-align-center u-clearfix u-grey-10 u-section-1" id="sec-357b">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <div class="u-align-center u-container-style u-group u-radius-50 u-shape-round u-white u-group-1">
+          <div class="u-container-layout u-valign-middle u-container-layout-1">
+            <h3 class="u-text u-text-default u-text-1">Sign In</h3>
+            
+            <?php
+                
+                if(isset($_SESSION['status'])){
+                    
+                    echo $_SESSION['status'];
+                }
+                
+                ?>
+            
+            <div class="u-expanded-width u-form u-login-control u-form-1">
+              <form action="" method="post" class="u-clearfix u-form-custom-backend u-form-spacing-35 u-form-vertical u-inner-form" source="custom" name="form-2" style="padding: 10px;">
+                <div class="u-form-group u-form-name">
+                  <label for="username-708d" class="u-form-control-hidden u-label"></label>
+                  <input type="text" placeholder="Enter your Username" id="username-708d" name="username" class="u-grey-5 u-input u-input-rectangle" required="">
+                  
+                  <h6 class="text-center" style="color:#ff0000"><?php echo $message_username; ?></h6>
+<!--                  <input type="email" name="email" class="form-control" placeholder="Email address">-->
+                </div>
+                <div class="u-form-group u-form-password">
+                  <label for="password-708d" class="u-form-control-hidden u-label"></label>
+                  <input type="password" placeholder="Enter your Password" id="password-708d" name="password" class="u-grey-5 u-input u-input-rectangle" required="">
+                  <h6 class="text-center" style="color:#ff0000"><?php echo $message_password; ?></h6>
+                </div>
+                <div class="u-form-checkbox u-form-group">
+                  <input type="checkbox" id="checkbox-708d" name="remember" value="On">
+                  <label for="checkbox-708d" class="u-label">Remember me</label>
+                </div>
+                <div class="u-align-center u-form-group u-form-submit">
+                  <a href="" class="u-btn u-btn-round u-btn-submit u-button-style u-radius-17 u-btn-1">Login</a>
+                  <input type="submit" name="submit" value="submit" class="u-form-control-hidden">
+<!--                  <button class="u-btn u-btn-round u-btn-submit u-button-style u-radius-17 u-btn-1" name="submit" type="submit" id ="submit">login</button>-->
+                </div>
+                <input type="hidden" value="" name="recaptchaResponse">
+              </form>
+            </div>
+            <a href="" class="u-border-1 u-border-active-palette-2-base u-border-hover-palette-1-base u-btn u-button-style u-login-control u-login-forgot-password u-none u-text-palette-1-base u-btn-2">Forgot password?</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-ff43"><div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="https://nicepage.com/" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
+    </section>
+  </body>
+</html>
