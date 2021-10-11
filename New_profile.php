4f705dc2554494d5534c5c33879a2de9e1524f6e
[priya111260] <[reshma.p@cognateglobal.com]>
2021-10-11 16:18:30 +0530

    Update

---
+++ New_profile.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="VR headsets, ​VR's Surprising Benefits, ​VR's Surprising Benefits, 1, 2, 3, ​​The benefits of virtual reality, ​Experts reveal four surprising health benefits of VR gaming, Follow Us!">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Page 1</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="New_profile.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.27.0, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    
+    
+    <script type="application/ld+json">{
+    "@context": "http://schema.org",
+    "@type": "Organization",
+    "name": "",
+    "logo": "images/default-logo.png"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Page 1">
+    <meta property="og:type" content="website">
+  </head>
+  <body class="u-body u-white"><header class="u-clearfix u-header u-header" id="sec-4ce4"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="https://nicepage.com" class="u-image u-logo u-image-1">
+          <img src="images/default-logo.png" class="u-logo-image u-logo-image-1">
+        </a>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol id="menu-hamburger" viewBox="0 0 16 16" style="width: 16px; height: 16px;"><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</symbol>
+</defs></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Home.html" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="View-All-Artists.html" style="padding: 10px 20px;">View All Artists</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" style="padding: 10px 20px;">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" style="padding: 10px 20px;">Contact</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Home.html" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="View-All-Artists.html" style="padding: 10px 20px;">View All Artists</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" style="padding: 10px 20px;">About</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" style="padding: 10px 20px;">Contact</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header> 
+    <section class="u-clearfix u-section-1" id="sec-f37b">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h2 class="u-text u-text-1">Kyle J<span style="font-weight: 700;"></span>ames
+        </h2>
+        <div class="u-align-left u-expanded-width-sm u-expanded-width-xs u-video u-video-1">
+          <div class="embed-responsive embed-responsive-1">
+            <iframe style="position: absolute;top: 0;left: 0;width: 100%;height: 100%;" class="embed-responsive-item" src="https://www.youtube.com/embed/L-hm3S1aSnI?mute=0&amp;showinfo=0&amp;controls=0&amp;start=0" frameborder="0" allowfullscreen=""></iframe>
+          </div>
+        </div>
+        <div class="u-border-2 u-border-grey-5 u-container-style u-group u-radius-8 u-shape-round u-white u-group-1">
+          <div class="u-container-layout u-container-layout-1">
+            <h6 class="u-text u-text-default u-text-2">Kyle James</h6>
+            <p class="u-text u-text-default u-text-3">"Healthiest Rapper Alive!"</p>
+            <h6 class="u-text u-text-default u-text-4">About</h6>
+            <p class="u-text u-text-default u-text-5">Music is my life! I love to create art and I’m here to inspire others! I grew up in Hartford, but moved to Phoenix, AZ shortly after my father died and started a new life. I got into rapping while I was<span class="u-text-body-color"></span> attending Arizona State University. I started getting more into it when I was going to Phoenix College taking music classes. Nowadays I’m involved with various forms of art. My purpose is to have a positive impact on the planet. I will live a life that would make my father proud and help millions of people live better lives.
+            </p>
+          </div>
+        </div>
+        <div class="u-border-2 u-border-grey-5 u-container-style u-group u-radius-8 u-shape-round u-group-2">
+          <div class="u-container-layout u-container-layout-2">
+            <h6 class="u-text u-text-6">Location: <span style="font-weight: normal;">Trichy</span>
+            </h6>
+            <h6 class="u-text u-text-7"><b>Home town: </b>Trichy
+            </h6>
+            <h6 class="u-text u-text-8"><b>Birth place: </b>Trichy
+            </h6>
+          </div>
+        </div>
+        <div class="u-border-2 u-border-grey-5 u-container-style u-group u-radius-8 u-shape-round u-group-3">
+          <div class="u-container-layout u-container-layout-3">
+            <h6 class="u-text u-text-black u-text-9"><b>Instagram: </b>www.instagram.com
+            </h6>
+            <h6 class="u-text u-text-10"><b>Youtube: </b>www.youtube.com
+            </h6>
+            <h6 class="u-text u-text-11">Spotify<b>: </b>
+              <span style="font-weight: normal;">www.spotify.com</span>
+            </h6>
+          </div>
+        </div>
+        <div class="u-border-2 u-border-grey-5 u-container-style u-group u-radius-8 u-shape-round u-group-4">
+          <div class="u-container-layout u-container-layout-4">
+            <h6 class="u-text u-text-12"><b>Merch</b>
+            </h6>
+            <a href="https://nicepage.com/website-mockup" class="u-btn u-button-style u-btn-1">Shop Now</a>
+          </div>
+        </div>
+        <div class="u-border-2 u-border-grey-5 u-container-style u-group u-radius-8 u-shape-round u-group-5">
+          <div class="u-container-layout u-container-layout-5">
+            <h6 class="u-text u-text-13"><b>featured Song:</b>
+            </h6>
+          </div>
+        </div>
+        <div class="u-border-2 u-border-grey-5 u-container-style u-group u-radius-8 u-shape-round u-group-6">
+          <div class="u-container-layout u-container-layout-6">
+            <h6 class="u-text u-text-14"><b>featured Music:</b>
+            </h6>
+          </div>
+        </div>
+        <div class="u-border-2 u-border-grey-5 u-container-style u-group u-radius-8 u-shape-round u-group-7">
+          <div class="u-container-layout u-container-layout-7">
+            <h6 class="u-text u-text-15"><b>featured Album:</b>
+            </h6>
+          </div>
+        </div>
+        <div class="u-border-2 u-border-grey-5 u-container-style u-group u-radius-8 u-shape-round u-group-8">
+          <div class="u-container-layout u-container-layout-8">
+            <h6 class="u-text u-text-16">&nbsp;<b>Dig Deeper!</b>
+              <br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Unlock Kyle James premium&nbsp;Profile and&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; access the following:<br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <span style="font-weight: 700;">Email address: </span>KyleJames860@gmail.com<br>
+              <br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;
+            </h6>
+            <a href="https://nicepage.com/css-templates" class="u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-base u-radius-50 u-btn-2">Upgrade Now ($10)<span style="font-weight: 700;"></span>
+            </a>
+          </div>
+        </div>
+        <div class="u-border-2 u-border-grey-5 u-radius-8 u-shape u-shape-round u-shape-1"></div>
+        <a href="https://nicepage.com/css-templates" class="u-border-none u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-light-2 u-radius-50 u-btn-3">30MIN<span style="font-weight: 400;">
+            <span style="font-weight: 700;">
+              <span style="font-weight: 400;"> call $50</span>
+            </span>
+          </span>
+        </a>
+        <h6 class="u-text u-text-17">&nbsp; &nbsp; &nbsp; &nbsp; Purchase&nbsp;Time with&nbsp;<br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Kyle James !
+        </h6>
+        <a href="https://nicepage.com/css-templates" class="u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-light-2 u-radius-50 u-btn-4">60min call $75</a>
+        <a href="https://nicepage.com/css-templates" class="u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-base u-radius-50 u-btn-5">Add to favorties</a>
+        <a href="https://nicepage.com/css-templates" class="u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-base u-radius-50 u-btn-6">Share profile</a>
+        <div class="u-border-2 u-border-grey-5 u-container-style u-group u-radius-8 u-shape-round u-group-9">
+          <div class="u-container-layout u-container-layout-9">
+            <div class="fr-view u-clearfix u-rich-text u-text u-text-18"></div>
+            <div class="fr-view u-clearfix u-rich-text u-text u-text-19">
+              <p>
+                <span style="font-weight: 700;">Additional Profile</span>
+              </p>
+            </div>
+            <h6 class="u-text u-text-20">
+              <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-1-base u-btn-7" href="https://www.linkedin.com">Linkedin</a>
+            </h6>
+            <h6 class="u-text u-text-21">
+              <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-1-base u-btn-8" href="https://nicepage.com">Soundcloud</a>
+            </h6>
+            <h6 class="u-text u-text-22">
+              <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-1-base u-btn-9" href="https://nicepage.com">Twitter</a>
+            </h6>
+            <h6 class="u-text u-text-23">
+              <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-palette-1-base u-btn-10" href="https://nicepage.com">Facebook</a>
+            </h6>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-a0d1"><div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-design" target="_blank">
+        <span>Website Design</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="https://nicepage.com/html-website-builder" target="_blank">
+        <span>HTML Layout generator</span>
+      </a>. 
+    </section>
+  </body>
+</html>
